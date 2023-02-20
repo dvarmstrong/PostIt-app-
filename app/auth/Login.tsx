f2402ac4f50cbfbdea482e05af018966f593fc57
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2023-02-20 15:53:02 -0500

    update

---
+++ app/auth/Login.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { signIn } from 'next-auth/react' ;
+
+import React from 'react'
+
+export default function Login () {
+    return (
+        <li className='list-none'>
+            <button 
+            onClick={() => signIn()} 
+            className='text-sm bg-gray-700 text-white py-2 px-6 rounded-xl disabled:opacity-25'>
+            Sign In 
+            </button>
+
+        </li>
+    )
+}
+
